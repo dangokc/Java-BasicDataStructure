fd8c593698753999720a2c3f13632d0c50c4e3b8
Huy Dang <dangokc@gmail.com>
2015-08-20 18:23:20 -0500

    Init new Project

---
+++ src/Runner.java
@@ -0,0 +1,34 @@
+
+public class Runner {
+
+    private int raceID;
+    private String name;
+    
+    public Runner() {
+        //do nothing.
+    }
+    
+    public int getRaceID() {
+        return raceID;
+    }
+    
+    public void setRaceID(int value) {
+        raceID = value;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String value) {
+        name = value;
+    }
+    
+    public String performSport() {
+        return "Running";
+    }
+    
+    public String toString() {
+        return String.format("%s:\t%d is %s\n", getName(), getRaceID(), performSport());
+    }
+}
---
+++ src/Swimmer.java
@@ -0,0 +1,39 @@
+
+public class Swimmer {
+
+    private int raceID;
+    private String name;
+    
+    public Swimmer() {
+        //do nothing.
+    }
+    
+    public Swimmer(int racerID, String swimmerName) {
+        raceID = racerID;
+        name = swimmerName;
+    }
+    
+    public int getRaceID() {
+        return raceID;
+    }
+    
+    public void setRaceID(int value) {
+        raceID = value;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String value) {
+        name = value;
+    }
+    
+    public String performSport() {
+        return "Swimming";
+    }
+    
+    public String toString() {
+        return String.format("%s:\t%d is %s\n", getName(), getRaceID(), performSport());
+    }
+}
